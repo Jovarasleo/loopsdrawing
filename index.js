b231e2bd1402d830b8855e00b4e60e770acb6d06
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2021-09-20 21:27:26 +0300

    5th version: added CSS

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ function inputValue(event) {
   } else aukstis = null;
 }
 const result = document.querySelector(".result");
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-const button3 = document.querySelector(".button3");
-const button4 = document.querySelector(".button4");
-const button5 = document.querySelector(".button5");
 function triangle() {
   for (i = 0; i <= aukstis; i++) {
     result.innerHTML += "<br>";
